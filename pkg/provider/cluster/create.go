a11f3f4655365b6a702f3fdc9f1586f0a971dcac
Cole Wippern <cgwippern@gmail.com>
2019-01-23 06:35:46 -0800

    cluster provider updates

---
+++ pkg/provider/cluster/create.go
@@ -0,0 +1,87 @@
+package cluster
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/rds"
+	log "github.com/sirupsen/logrus"
+)
+
+// NewDBClusterInput contains the data to be used when creating the RDS cluster
+type NewDBClusterInput struct {
+	// Identifier for the RDS cluster
+	ClusterId string
+	// Engine type
+	Engine string
+	// Engine version (optional)
+	EngineVersion string
+	// User name for root DB user
+	MasterUsername string
+	// Password for root DB user
+	MasterUserPass string
+	// List of Security groups to attach to the RDS cluster
+	SecurityGroupIds []string
+	// Name of the RDS Subnet Group to use
+	SubnetGroupName string
+	// Name of RDS Cluster Parameter Group to use (optional)
+	ParameterGroupName string
+	// List of Availability Zones in which to deploy the RDS cluster (optional)
+	AvailabilityZones []string
+	// Length of time (in days) to retain backups (optional)
+	BackupRetentionPeriod int64
+	// Whether the DB data should be encrypted at rest (optional)
+	StorageEncrypted bool
+}
+
+func CreateDBCluster(svc *rds.RDS, input NewDBClusterInput) (*rds.DBCluster, error) {
+	return createRDSCluster(svc, NewCreateClusterInput(input))
+}
+
+func NewCreateClusterInput(input NewDBClusterInput) *rds.CreateDBClusterInput {
+	sIds := make([]*string, 0)
+	for _, i := range input.SecurityGroupIds {
+		sIds = append(sIds, aws.String(i))
+	}
+
+	azs := make([]*string, 0)
+	for _, i := range input.AvailabilityZones {
+		azs = append(azs, aws.String(i))
+	}
+
+	clusterInput := &rds.CreateDBClusterInput{
+		DBClusterIdentifier: aws.String(input.ClusterId),
+		Engine:              aws.String(input.Engine),
+		MasterUsername:      aws.String(input.MasterUsername),
+		MasterUserPassword:  aws.String(input.MasterUserPass),
+		DBSubnetGroupName:   aws.String(input.SubnetGroupName),
+		VpcSecurityGroupIds: sIds,
+		StorageEncrypted:    aws.Bool(input.StorageEncrypted),
+	}
+
+	if input.EngineVersion != "" {
+		clusterInput.EngineVersion = aws.String(input.EngineVersion)
+	}
+
+	if input.ParameterGroupName != "" {
+		clusterInput.DBClusterParameterGroupName = aws.String(input.ParameterGroupName)
+	}
+
+	if len(azs) > 0 {
+		clusterInput.AvailabilityZones = azs
+	}
+
+	if input.BackupRetentionPeriod > 0 {
+		clusterInput.BackupRetentionPeriod = aws.Int64(input.BackupRetentionPeriod)
+	}
+
+	return clusterInput
+}
+
+func createRDSCluster(svc *rds.RDS, clusterInput *rds.CreateDBClusterInput) (*rds.DBCluster, error) {
+	clusterOutput, err := svc.CreateDBCluster(clusterInput)
+	if err != nil {
+		log.Warn(err)
+		return nil, err
+	}
+
+	return clusterOutput.DBCluster, nil
+}
---
+++ pkg/provider/cluster/update.go
@@ -0,0 +1,112 @@
+package cluster
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/rds"
+	log "github.com/sirupsen/logrus"
+)
+
+type UpdateDBClusterRequest struct {
+	cluster            *rds.DBCluster
+	engineVersion      *string
+	masterUserPass     *string
+	securityGroupIds   []*string
+	parameterGroupName *string
+}
+
+func (u *UpdateDBClusterRequest) SetCluster(v *rds.DBCluster) *UpdateDBClusterRequest {
+	u.cluster = v
+	return u
+}
+
+func (u *UpdateDBClusterRequest) SetEngineVersion(v string) *UpdateDBClusterRequest {
+	u.engineVersion = aws.String(v)
+	return u
+}
+
+func (u *UpdateDBClusterRequest) SetMasterUserPass(v string) *UpdateDBClusterRequest {
+	u.masterUserPass = aws.String(v)
+	return u
+}
+
+func (u *UpdateDBClusterRequest) SetSecurityGroupIds(v []string) *UpdateDBClusterRequest {
+	sIds := make([]*string, 0)
+	for _, i := range v {
+		sIds = append(sIds, aws.String(i))
+	}
+
+	u.securityGroupIds = sIds
+	return u
+}
+
+func (u *UpdateDBClusterRequest) SetParameterGroupName(v string) *UpdateDBClusterRequest {
+	u.parameterGroupName = aws.String(v)
+	return u
+}
+
+func UpdateDBCluster(svc *rds.RDS, req *UpdateDBClusterRequest) (*rds.DBCluster, error) {
+	input := &rds.ModifyDBClusterInput{
+		ApplyImmediately:            aws.Bool(true),
+		DBClusterIdentifier:         req.cluster.DBClusterIdentifier,
+		VpcSecurityGroupIds:         req.securityGroupIds,
+		DBClusterParameterGroupName: req.parameterGroupName,
+	}
+
+	if *req.cluster.EngineVersion != *req.engineVersion {
+		input.EngineVersion = req.engineVersion
+	}
+
+	if req.masterUserPass != nil && *req.masterUserPass != "" {
+		input.MasterUserPassword = req.masterUserPass
+	}
+
+	result, err := svc.ModifyDBCluster(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case rds.ErrCodeDBClusterNotFoundFault:
+				log.Warn(rds.ErrCodeDBClusterNotFoundFault, aerr.Error())
+				return nil, aerr
+			case rds.ErrCodeInvalidDBClusterStateFault:
+				log.Warn(rds.ErrCodeInvalidDBClusterStateFault, aerr.Error())
+				return nil, aerr
+			case rds.ErrCodeStorageQuotaExceededFault:
+				log.Warn(rds.ErrCodeStorageQuotaExceededFault, aerr.Error())
+				return nil, aerr
+			case rds.ErrCodeDBSubnetGroupNotFoundFault:
+				log.Warn(rds.ErrCodeDBSubnetGroupNotFoundFault, aerr.Error())
+				return nil, aerr
+			case rds.ErrCodeInvalidVPCNetworkStateFault:
+				log.Warn(rds.ErrCodeInvalidVPCNetworkStateFault, aerr.Error())
+				return nil, aerr
+			case rds.ErrCodeInvalidDBSubnetGroupStateFault:
+				log.Warn(rds.ErrCodeInvalidDBSubnetGroupStateFault, aerr.Error())
+				return nil, aerr
+			case rds.ErrCodeInvalidSubnet:
+				log.Warn(rds.ErrCodeInvalidSubnet, aerr.Error())
+				return nil, aerr
+			case rds.ErrCodeDBClusterParameterGroupNotFoundFault:
+				log.Warn(rds.ErrCodeDBClusterParameterGroupNotFoundFault, aerr.Error())
+				return nil, aerr
+			case rds.ErrCodeInvalidDBSecurityGroupStateFault:
+				log.Warn(rds.ErrCodeInvalidDBSecurityGroupStateFault, aerr.Error())
+				return nil, aerr
+			case rds.ErrCodeInvalidDBInstanceStateFault:
+				log.Warn(rds.ErrCodeInvalidDBInstanceStateFault, aerr.Error())
+				return nil, aerr
+			case rds.ErrCodeDBClusterAlreadyExistsFault:
+				log.Warn(rds.ErrCodeDBClusterAlreadyExistsFault, aerr.Error())
+				return nil, aerr
+			default:
+				log.Warn(aerr.Error())
+				return nil, aerr
+			}
+		} else {
+			log.Warn(err)
+			return nil, err
+		}
+	}
+
+	return result.DBCluster, nil
+}
